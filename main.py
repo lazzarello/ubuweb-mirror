08070b288ef7de64e7df15be25726a12f2a32a46
Lee Azzarello <lee.azzarello@astraspace.com>
2022-06-03 21:24:40 -0700

    download random artist's works

--- main.py
+++ main.py
@@ -15,18 +15,20 @@ def download_random_work_from(artists):
     work.download_work()
 
 def download_all_works_from(artist):
+    print(artist)
     page = Page()
     artist_works = page.get_artist_works(artist)
     for work in artist_works:
         work.set_download_url(work)
         if work.download_url:
             work.download_work()
-    
+
 def main():
     page = Page()
     artists_page = page.get_artists(FILM_URL)
+    r = len(artists_page)
     # download_random_work_from(artists_page)
-    download_all_works_from(artists_page[0])
+    download_all_works_from(artists_page[random.choice(range(r))])
 
 if __name__ == "__main__":
     main()
