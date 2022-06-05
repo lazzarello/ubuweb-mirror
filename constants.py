bbb056be06cc653b7851595361ae499947a2b928
Lee Azzarello <lee.azzarello@astraspace.com>
2022-06-05 03:01:38 -0700

    add a list of broken page ids

--- constants.py
+++ constants.py
@@ -3,3 +3,13 @@ FILM_URL = "https://www.ubu.com/film/index.html"
 BASE_FILM_URL = "https://www.ubu.com/film/"
 DOWNLOAD_PATH = "/home/lee/Videos/ubuweb/"
 ERROR_URL = "https://www.memoryoftheworld.org"
+BROKEN_PAGES=[215,15,9]
+'''
+215
+https://www.ubu.com/film/clarke_ornette.html
+this index uses Javascript to render the link to media. the
+streaming video uses a service called https://criticalcommons.org/embed?m=fwqF8eomo
+which is not valid in youtube-dl
+15 page not found, redirect
+9 dmca takedown, zero works.
+'''
